d4e26f669ac4340afefb19c77c40d5222ef97d95
Ethan Clark <elc3@students.calvin.edu>
2017-02-15 12:32:04 -0500

    Initial commit

---
+++ Practice/hello.c
@@ -0,0 +1,8 @@
+#import <stdio.h>
+
+int main()
+{
+	printf("Hello world!\n");
+
+	return 0;
+}
---
+++ Project1/prank.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+char * temp_msg;
+int temp_sec;
+int new_time = 0;
+int new_msg = 0;
+int enable_int = 0;
+
+void annoying_message(int sig) {
+	if (new_msg == 1) {
+		printf("%s\n", temp_msg);
+	}
+	else {
+		printf("THIS MESSAGE IS ANNOYING\n");
+	}
+	signal(SIGALRM, annoying_message);
+	if (new_time == 1) {
+		alarm(temp_sec);
+	}
+	else {
+		alarm(5);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "+i") == 0)
+		{
+			enable_int = 1;
+			printf("Processed +i argument\n");
+		}
+
+		if (strcmp(argv[i], "-m") == 0)
+		{
+			temp_msg = argv[i+1];
+			new_msg = 1;
+		}
+
+		if (strcmp(argv[i], "+t") == 0)
+		{
+			temp_sec = atoi(argv[i+1]);
+			new_time = 1;
+		}
+
+	}
+
+	signal(SIGALRM, annoying_message);
+	if (new_time == 1) {
+		alarm(new_time);
+	}
+	else {
+		alarm(5);
+	}
+
+	//Infinite loop to keep the program running
+	while (1);
+
+}
