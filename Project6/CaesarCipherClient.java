7493414e782d978e888b0b77cfaf89e12ce6d806
Ethan Clark <elc3@students.calvin.edu>
2017-05-15 16:25:53 -0400

    Almost finished project6

--- Project6/CaesarCipherClient.java
+++ Project6/CaesarCipherClient.java
@@ -4,7 +4,7 @@
  *
  * Written by: Ethan Clark (elc3)
  * CS 232 - Project6
- * Date: May 11, 2017
+ * Date: May 15, 2017
  */
 
 import java.util.Scanner;
@@ -19,10 +19,17 @@ public class CaesarCipherClient {
 
 	public static void main(String[] args) throws IOException {
 
+		// Make sure there are two arguments from the user
+		if (args.length != 2) {
+            System.err.println("Wrong number of arguments - Need 2 arguments");
+            System.exit(1);
+        }
+
 		// Get the portNumber and the server name from the argument list
 		Integer portNumber;
 		String server_name = args[0];
 
+		// Make sure the second argument was an integer
 		try {
 			portNumber = Integer.parseInt(args[1]);
 		} catch (NumberFormatException ex) {
@@ -30,55 +37,59 @@ public class CaesarCipherClient {
 			return;
 		}
 
-
 		// Welcome the user and prompt for an integer offset value
 		System.out.println("Welcome to Ethan's CasesarCipherClient!\n");
 
-		System.out.println("Please enter the rotation offset integer (1..25): ");
+		System.out.print("Please enter the rotation offset integer (1..25): ");
 
 		// Read in the integer value from the user
 		Scanner getInput = new Scanner(System.in);
 		Integer rotation = getInput.nextInt();
 
 		// Create the necessary server sockets and connections to the CeaserCipherServer
-		Socket serverSocket = new Socket(server_name, portNumber);
-		OutputStream os = serverSocket.getOutputStream();
-		PrintWriter out = new PrintWriter(os, true);
-
-		// Write the integer offset to the CaesarCipherServer
-		out.append(rotation.toString());
-		out.append("\n");
-		out.flush();
-
-		// Read in the returned offset integer value from the CaesarCipherServer
-		BufferedReader input = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-		String outputLine = input.readLine();
-		System.out.println("Server returned integer of: " + outputLine + "\n");
-
-		/*
-		 * Continually prompt for the user to enter text
-		 * Send the text to the CaesarCipherServer to be encrypted
-		 * Read back the encrypted text and display to the user
-		 *
-		 * If the user enters "quit", quit the application
-		 */
-		String returned;
-		while (true) {
-			System.out.println("Please enter the text to be encrypted: ");
-			String text = getInput.nextLine();
-
-			if (text.equals("quit")) {
-				System.out.println("\nThanks for using the CaeserCipher!\n");
-				break;
-			}
-
-			out.append(text);
+		try (
+			Socket serverSocket = new Socket(server_name, portNumber);
+			OutputStream os = serverSocket.getOutputStream();
+			PrintWriter out = new PrintWriter(os, true);
+			BufferedReader input = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+		) {
+			// Write the integer offset to the CaesarCipherServer
+			out.append(rotation.toString());
 			out.append("\n");
 			out.flush();
 
-			returned = input.readLine();
+			// Read in the returned offset integer value from the CaesarCipherServer
+			String outputLine = input.readLine();
+			System.out.println("Server returned integer of: " + outputLine + "\n");
 
-			System.out.println("Returned text is: " + returned);
+			/*
+		 	* Continually prompt for the user to enter text
+		 	* Send the text to the CaesarCipherServer to be encrypted
+		 	* Read back the encrypted text and display to the user
+		 	*
+		 	* If the user enters "quit", quit the application
+		 	*/
+			String returned;
+			while (true) {
+				System.out.print("Please enter the text to be encrypted: ");
+				String text = getInput.nextLine();
+
+				if (text.equals("quit")) {
+					System.out.println("\nThanks for using the CaeserCipher!\n");
+					break;
+				}
+
+				out.append(text);
+				out.append("\n");
+				out.flush();
+
+				returned = input.readLine();
+
+				System.out.println("Returned text is: " + returned + "\n");
+			}
+		} catch (IOException e) {
+			System.err.println("Could not connect to server " + server_name);
+			System.exit(-1);
 		}
 
 	}
--- Project6/CaesarCipherServer.java
+++ Project6/CaesarCipherServer.java
@@ -4,7 +4,7 @@
  *
  * Written by: Ethan Clark
  * Project 6 - CS 232
- * Date: May 11, 2017
+ * Date: May 15, 2017
  */
 
 import java.util.Scanner;
@@ -14,59 +14,104 @@ import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class CaesarCipherServer {
 
 	public static void main(String[] args) throws Exception {
 
+		// Check that there was one argument from the user
+		if (args.length != 1) {
+            System.err.println("Usage: java server <port number>");
+            System.exit(1);
+        }
+
+        // Get the port number from the user
 		int portNumber = Integer.parseInt(args[0]);
 		System.out.println("Server started on port " + portNumber);
-		boolean listening = true;
 
-		try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-			while (listening) {
-				new CaesarCipherMultiThread(serverSocket.accept()).start();
-			}
-		} catch (IOException e) {
-			System.err.println("Could not listen on port " + portNumber);
-			Systemexit(-1);
-		}
+		// Create a forever loop variable to wait for new connections to the server
+		Boolean listening = true;
+
+		// Try connecting server to the given port number...
+		try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+
+			// Continuously wait for new connections to the server and spawn off new threads
+            while (listening) {
+	            new CaesarCipherMultiThread(serverSocket.accept()).start();
+	        }
+
+	    // If the server is unable to use the given port number...
+	    } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
+
+// Class that implements a new spawned off thread for the server
+class CaesarCipherMultiThread extends Thread {
+
+	// Instance variable for the client socket connection
+	private Socket clientSocket = null;
+	private String ipAddress;
+
+	// Assign the connection from the main class to the spawned off thread class
+	// Also display date, time, and IP Address of connected client
+	public CaesarCipherMultiThread(Socket socket) {
+		this.clientSocket = socket;
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		ipAddress = this.clientSocket.getRemoteSocketAddress().toString();
+		System.out.println("New connection to " + ipAddress + " at " + dateFormat.format(date));
 	}
 
-	public class CaesarCipherMultiThread extends Thread {
-		private Socket socket = null;
+	// Run the forever loop, prompting for user input and encrypting the input
+	public void run() {
 
-		public CaesarCipherMultiThread(Socket socket) {
-			super("CaesarCipherMultiThread");
-			this.socket = socket;
-		}
-
-		public void run() {
+		try (   PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		) {
+			// Boolean to know if the user has already entered a rotation value
+			Boolean integer = true;
 			Integer rotation = 0;
-			try (
-				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			) {
-				String string;
-				while ((s = in.readLine()) != null) {
-					rotation = Integer.parseInt(s);
+			String inputline;
+	
+			// While the server can read in input from the client
+			while ((inputline = in.readLine()) != null) {
+
+				// If user just connected, they need to give a rotation value
+				if (integer) {
+					rotation = Integer.parseInt(inputline);
 					out.append(rotation.toString());
 					out.append("\n");
 					out.flush();
-					out.append(caesarCipher(rotation, string));
-					out.append("\n");
-					out.flush();
+					integer = false;
+					continue;
 				}
 
-				out.close();
-				in.close();
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+				// Call caesarCipher function to encrypt the given text
+				out.append(caesarCipher(rotation, inputline));
+				out.append("\n");
+				out.flush();
 			}
+
+			// Once the client quits, close connection to client and stop running thread
+			System.out.println("Closing connection to client " + ipAddress);
+			out.close();
+			in.close();
+			clientSocket.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
+
+	// caesarCipher function to encrypt text based on a rotation value
+	//		Making sure to loop back around to the beginning of the alphabet if the rotation brings to the end of the alphanet
 	private static String caesarCipher(Integer rotation, String string) {
 
 		String tempString = "";
